669b5738981a7589fabb5c5aae767040150c6a30
GabePurc <151047953+GabePurc@users.noreply.github.com>
2026-09-07 21:08:28 -0400

    Initial Commit

---
+++ Blank Space/Blank_SpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blank_SpaceApp.swift
+//  Blank Space
+//
+//  Created by Gabe Purcell on 9/7/26.
+//
+
+import SwiftUI
+
+@main
+struct Blank_SpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
